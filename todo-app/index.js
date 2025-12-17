adcabed2a8f7dc46b9c2c0072d9b7152f8531209
OzKanoo <student@example.com>
2025-12-17 20:29:09 +0100

    Add Exercise 1.2 Todo App finally

---
+++ todo-app/index.js
@@ -0,0 +1,9 @@
+﻿const http = require('http');
+const port = process.env.PORT || 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.end('Hello from Todo App\n');
+});
+server.listen(port, () => {
+  console.log('Server started in port ' + port);
+});
