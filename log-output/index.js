7bb0cd13d40306139cd9c327aed24c45ddcae14a
OzKanoo <student@example.com>
2025-12-17 22:25:57 +0100

    Exercise 2.1: Connect apps via HTTP instead of files

--- log-output/index.js
+++ log-output/index.js
@@ -1,16 +1,25 @@
 ﻿const http = require('http');
-const fs = require('fs');
-const path = require('path');
 const crypto = require('crypto');
-
-const pongPath = path.join(__dirname, 'files', 'pong.txt');
 const randomString = crypto.randomBytes(10).toString('hex');
 
-http.createServer((req, res) => {
+// Hilfsfunktion für den HTTP-Request an die andere App
+const getPongs = () => {
+  return new Promise((resolve) => {
+    // WICHTIG: Wir nutzen den DNS-Namen des Services 'ping-pong-svc'
+    http.get('http://ping-pong-svc:80/pongs', (res) => {
+      let data = '';
+      res.on('data', (chunk) => data += chunk);
+      res.on('end', () => resolve(data));
+    }).on('error', (err) => {
+      console.error('Error fetching pongs:', err.message);
+      resolve('0');
+    });
+  });
+};
+
+http.createServer(async (req, res) => {
+  const pongs = await getPongs();
   const timestamp = new Date().toISOString();
-  let pongs = '0';
-  if (fs.existsSync(pongPath)) {
-    pongs = fs.readFileSync(pongPath, 'utf8');
-  }
+  res.setHeader('Content-Type', 'text/plain');
   res.end(\: \.\nPings / Pongs: \);
 }).listen(3000);
--- ping-pong/index.js
+++ ping-pong/index.js
@@ -1,19 +1,13 @@
 ﻿const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const filePath = path.join(__dirname, 'files', 'pong.txt');
-
 let counter = 0;
-// Falls die Datei schon existiert (nach Neustart), Zähler laden
-if (fs.existsSync(filePath)) {
-  counter = parseInt(fs.readFileSync(filePath, 'utf8')) || 0;
-}
 
 http.createServer((req, res) => {
   if (req.url === '/pingpong') {
     counter++;
-    fs.writeFileSync(filePath, counter.toString()); // In Datei speichern
     res.end('pong ' + (counter - 1));
+  } else if (req.url === '/pongs') {
+    // Dieser Endpunkt ist nur für die Log-App gedacht
+    res.end(counter.toString());
   } else {
     res.statusCode = 404;
     res.end();
