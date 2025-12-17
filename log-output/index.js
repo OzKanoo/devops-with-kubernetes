fe09c8021d1fceb167b1efdab1dbcf470a569b20
OzKanoo <student@example.com>
2025-12-17 21:59:23 +0100

    Exercise 1.7: Ingress and Service for log-output

--- log-output/index.js
+++ log-output/index.js
@@ -1,8 +1,21 @@
-﻿const { v4: uuidv4 } = require('uuid');
-const id = uuidv4();
+﻿const http = require('http');
+const crypto = require('crypto');
 
-console.log('Log Output App started. ID:', id);
+const randomString = crypto.randomBytes(10).toString('hex');
+const port = process.env.PORT || 3000;
 
-setInterval(() => {
-    console.log(new Date().toISOString() + ': ' + id);
-}, 5000);
+const server = http.createServer((req, res) => {
+  const timestamp = new Date().toISOString();
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(\: \);
+});
+
+server.listen(port, () => {
+  console.log('Server started in port ' + port);
+  
+  // Wir behalten die Konsolen-Ausgabe alle 5 Sekunden bei
+  setInterval(() => {
+    console.log(\: \);
+  }, 5000);
+});
