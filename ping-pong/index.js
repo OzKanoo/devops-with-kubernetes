ae0c44c557e1f8cf028757f451eb96eaef1deb96
OzKanoo <student@example.com>
2025-12-17 22:06:40 +0100

    Exercise 1.9: Add Ping-Pong app with shared Ingress

---
+++ ping-pong/index.js
@@ -0,0 +1,18 @@
+﻿const http = require('http');
+let counter = 0;
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/pingpong') {
+    res.statusCode = 200;
+    res.end('pong ' + counter);
+    counter++;
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
+});
+
+server.listen(port, () => {
+  console.log('Ping-Pong server started on port ' + port);
+});
