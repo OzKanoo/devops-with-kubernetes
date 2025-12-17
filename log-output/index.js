9fae743784d055269c09112edd0bd6a9c7b2d367
OzKanoo <student@example.com>
2025-12-17 20:00:05 +0100

    Abgabe Übung 1.1

---
+++ log-output/index.js
@@ -0,0 +1,8 @@
+﻿const { v4: uuidv4 } = require('uuid');
+const id = uuidv4();
+
+console.log('Log Output App started. ID:', id);
+
+setInterval(() => {
+    console.log(new Date().toISOString() + ': ' + id);
+}, 5000);
