54880f17b4742bbc9a2d95e37ccbaa6b64a0bf75
OzKanoo <student@example.com>
2025-12-17 22:53:30 +0100

    Exercise 3.4: Implemented URL rewriting with Gateway API

--- ping-pong/index.js
+++ ping-pong/index.js
@@ -4,14 +4,10 @@ const client = new Client({ connectionString: process.env.DATABASE_URL });
 client.connect().catch(err => console.error(err));
 
 http.createServer(async (req, res) => {
-  if (req.url === '/') { 
-    res.end('OK'); // Wichtig für GKE Health Check!
-  } else if (req.url === '/pingpong') {
+  // Die App antwortet jetzt nur noch auf dem Root-Pfad /
+  if (req.url === '/') {
     const result = await client.query('UPDATE pongs SET count = count + 1 WHERE id = 1 RETURNING count');
     res.end('pong ' + (result.rows[0].count - 1));
-  } else if (req.url === '/pongs') {
-    const result = await client.query('SELECT count FROM pongs WHERE id = 1');
-    res.end(result.rows[0].count.toString());
   } else {
     res.statusCode = 404; res.end();
   }
