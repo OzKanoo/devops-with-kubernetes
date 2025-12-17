7fb8812bfb699513746e430c91d6d8e2171d47a5
OzKanoo <student@example.com>
2025-12-17 22:42:41 +0100

    Exercise 2.9: Hourly CronJob to add random Wikipedia articles as todos

---
+++ todo-job/script.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/sh
+# 1. Zufällige Wikipedia-URL holen (nur den Location-Header)
+WIKI_URL=\
+
+echo "Found random article: \"
+
+# 2. Die URL als Todo an das Backend senden
+# Wir nutzen den DNS-Namen unseres Backend-Services im Namespace 'project'
+curl -X POST http://todo-backend-svc.project.svc.cluster.local:80/todos \
+     -H "Content-Type: application/json" \
+     -d "{\"todo\": \"Read \\"}"
